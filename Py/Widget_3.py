b44c38dda3da6d1a65d03acb9d51578715cbd9bb
Mikhail_Z <m.zimnev@yandex.ru>
2023-03-28 10:12:49 +0300

    Lesson 28

--- Py/Widget_3.py
+++ Py/Widget_3.py
@@ -1,10 +1,11 @@
 """ Пример программы по взаимодействию PushButton, LineEdit, Label"""
+""" Пример программы по взаимодействию PushButton, LineEdit, Label"""
 
 import sys
 from PyQt6.QtWidgets import (
     QMainWindow, QApplication,
-    QLabel, QCheckBox, QComboBox, QLineEdit, QPushButton,
-    QLineEdit, QSpinBox, QDoubleSpinBox, QSlider, QHBoxLayout, QVBoxLayout, QWidget,
+    QLabel, QCheckBox, QComboBox, QLineEdit, QPushButton, QTextEdit,
+    QLineEdit, QSpinBox, QDoubleSpinBox, QSlider, QHBoxLayout, QVBoxLayout, QWidget
 )
 from PyQt6.QtCore import Qt
 
@@ -20,20 +21,21 @@ class MainWindow(QMainWindow):
 
         layout = QVBoxLayout()
 
-        widget0 = QLabel("Введите число, ") # Label "Введите число"
+        widget0 = QLabel("Введите выражение, ") # Label "Введите число"
         font = widget0.font()
-        font.setPointSize(20)
+        font.setPointSize(10)
         widget0.setFont(font)
         widget0.setAlignment(Qt.AlignmentFlag.AlignHCenter | Qt.AlignmentFlag.AlignVCenter)
 
+
         widget1 = QLabel("нажмите Enter")  # Label "Нажимете Enter"
         font = widget1.font()
-        font.setPointSize(20)
+        font.setPointSize(10)
         widget1.setFont(font)
         widget1.setAlignment(Qt.AlignmentFlag.AlignHCenter | Qt.AlignmentFlag.AlignVCenter)
 
         self.widget2 = QLineEdit() # LineEdit для ввода текста
-        self.widget2.setMaxLength(10)
+        self.widget2.setMaxLength(20)
 
         # widget.setReadOnly(True) # раскомментируйте, чтобы сделать доступным только для чтения
 
@@ -47,9 +49,12 @@ class MainWindow(QMainWindow):
         button.setCheckable(True)
         button.clicked.connect(self.the_button_was_clicked)
 
-        self.label_result = QLabel() # Label для получения результата
+        self.label_result = QLabel() # Label для дублирования введенной строки
 
-        widgets = [widget0, widget1, self.widget2, button, self.label_result]
+        self.qte = QTextEdit() # Виджет для получения результата                    ####################################
+        self.qte.append("Здесь будет результат")                                    ####################################
+
+        widgets = [widget0, widget1, self.widget2, button, self.label_result, self.qte]  ################################
         for w in widgets:
             layout.addWidget(w)
 
@@ -75,6 +80,13 @@ class MainWindow(QMainWindow):
     def the_button_was_clicked(self):
         print("Clicked!")
         self.label_result.setText(self.text)
+        #####################################################################################
+        try:
+            res = str(eval(self.text)) # Заодно проверяется возможно ли введенное действие
+            self.qte.append(res)
+        except:
+            pass
+        #####################################################################################
         if self.tf:
             self.setWindowTitle('Result')
             self.tf = False
